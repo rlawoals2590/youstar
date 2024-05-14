f91b912680a5f6c673f5d52a67dc1d9b69f55fa6
김재민 <kimjaemin@gimjaemins-MacBook-Air.local>
2024-05-15 07:28:41 +0900

    [frontend] add: svelte router setting and config main page

---
+++ frontend/src/routes/Home.svelte
@@ -0,0 +1,14 @@
+<head>
+    <title>YOUSTAR</title>
+    <script src="https://kit.fontawesome.com/c087d0c2fc.js" crossorigin="anonymous"></script>
+</head>
+<div class="container">
+    <div class="content">
+        <div class="logo">YOUSTAR</div>
+        <div class="subtitle">Upload your photos and share them with others!</div>
+        <div class="buttons">
+            <a href="/" class="button">Post a photo <i class="fa-solid fa-chevron-right"></i></a>
+            <a href="/" class="button">View other photos <i class="fa-solid fa-chevron-right"></i></a>
+        </div>
+    </div>
+</div>
